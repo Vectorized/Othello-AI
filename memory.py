84d3af5f10d4e2c87ac3c430625e4def4ee84daf
webby1111 <webby1111@hotmail.com>
2015-08-25 23:53:50 +0800

    updated memory to support newer psutil

--- memory.py
+++ memory.py
@@ -1,7 +1,15 @@
 def getMemoryUsedMB():
     try:
         import os, psutil
+    except:
+        return 0
+    try:
         proc = psutil.Process(os.getpid())
         return proc.get_memory_info()[0] / float(2**20)
     except:
-        return 0
+        try:
+            proc = psutil.Process(os.getpid())
+            return proc.get_memory()[0] / float(2**20)
+        except:
+            return 0
+    return 0 
